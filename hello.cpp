82e240d2fb1792264226042168560f1aec2ce455
Mercury <2818676426@qq.com>
2021-03-14 16:14:59 +0800

    Add Test

---
+++ hello.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class work
+{
+public:
+    work operator +(const work &b)
+    {
+        work w;
+        w.length = this->length + b.length; 
+        return w;
+    }
+
+    int getLength()
+    {
+        return length;
+        }
+
+    int length;
+};
+
+int max(double f1, double f2)
+{
+    return 1;
+}
+
+int max(int n1, int n2)
+{
+    return 2;
+}
+
+int main(void)
+{
+    work t, u;
+    t.length = 10;
+    u.length = 5;
+    cout << t.length << " " << t.getLength() << "\n";
+    work v;
+    v = t + u;
+    cout << v.getLength() << "\n";
+    return 0;
+}
+
+// D:/Sublime Text 3/sublime_text.exe [file name]:[line number]
